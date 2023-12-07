c8266edc003b88354a705f407733c1f8fb0db70b
Yi-Chun (Rita) Wang <97589464+ritaycw@users.noreply.github.com>
2023-12-06 22:25:37 -0500

    Add files via upload

--- js/contactForm.js
+++ js/contactForm.js
@@ -1,60 +1,38 @@
-// function submitForm() {
-    // var name = document.getElementById("name").value;
-//     var phone = document.getElementById("phone").value;
-//     var email = document.getElementById("email").value;
-//     var comments = document.getElementById("comments").value;
+function validateForm() {
+  var name = document.getElementById("name").value;
+  var phone = document.getElementById("phone").value;
+  var email = document.getElementById("email").value;
+  var comments = document.getElementById("comments").value;
 
-//     var contactByPhone = document.getElementById("contactByPhone").checked;
-//     var contactByEmail = document.getElementById("contactByEmail").checked;
+  if (name === '') {
+      document.getElementById("nameError").style.display = "block";
+      return false;
+  } else {
+      document.getElementById("nameError").style.display = "none";
+  }
 
-//     console.log("Name: " + name);
-//     console.log("Phone: " + phone);
-//     console.log("Email: " + email);
-//     console.log("Contact by Phone: " + contactByPhone);
-//     console.log("Contact by Email: " + contactByEmail);
-//     console.log("Comments: " + comments);
-//     console.log("Contact By Phone: " + contactByPhone);
-//     console.log("Contact By Email: " + contactByEmail);
+  if (phone === '') {
+      document.getElementById("phoneError").style.display = "block";
+      return false;
+  } else {
+      document.getElementById("phoneError").style.display = "none";
+  }
 
-//     document.getElementById("contactForm").reset();
-//   }
+  if (email === '') {
+      document.getElementById("emailError").style.display = "block";
+      return false;
+  } else {
+      document.getElementById("emailError").style.display = "none";
+  }
 
+  if (comments === '') {
+      document.getElementById("commentsError").style.display = "block";
+      return false;
+  } else {
+      document.getElementById("commentsError").style.display = "none";
+  }
 
-  function validateForm() {
-    var name = document.getElementById("name").value;
-    var phone = document.getElementById("phone").value;
-    var email = document.getElementById("email").value;
-    var comments = document.getElementById("comments").value;
-
-    if (name === '') {
-        document.getElementById("nameError").style.display = "block";
-        return false;
-    } else {
-        document.getElementById("nameError").style.display = "none";
-    }
-
-    if (phone === '') {
-        document.getElementById("phoneError").style.display = "block";
-        return false;
-    } else {
-        document.getElementById("phoneError").style.display = "none";
-    }
-
-    if (email === '') {
-        document.getElementById("emailError").style.display = "block";
-        return false;
-    } else {
-        document.getElementById("emailError").style.display = "none";
-    }
-
-    if (comments === '') {
-        document.getElementById("commentsError").style.display = "block";
-        return false;
-    } else {
-        document.getElementById("commentsError").style.display = "none";
-    }
-
-    return true;
+  return true;
 }
 
 function submitForm() {
